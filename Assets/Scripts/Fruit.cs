6cc34bf1c4fbe9599eefe9ef103108c1d22afef8
Code New <codenew0@gmail.com>
2019-11-20 10:43:31 +0900

    new files

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameObject fruitSlicePrefab;
+    public float startForce = 15f;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * startForce, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D col) 
+    {
+        if (col.tag == "Blade")
+        {
+        //     Vector3 direction = (col.transform.position - transform.position).normalized;
+        //     Quaternion rotation = Quaternion.LookRotation(direction);
+        //     Instantiate(fruitSlicePrefab, transform.position, rotation);
+            GameObject slicedFruit = Instantiate(fruitSlicePrefab, transform.position, transform.rotation);
+            Destroy(slicedFruit, 3f);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/FruitSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour
+{
+    public GameObject fruitPrefab;
+    public Transform[] spawnPoints;
+    public float minDelay = 0.1f;
+    public float maxDelay = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnFruits());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnFruits()
+    {
+        while (true)
+        {
+            float delay = Random.Range(minDelay, maxDelay);
+            yield return new WaitForSeconds(1f);
+
+            int spawnIndex = Random.Range(0, spawnPoints.Length);
+            Transform spawnPoint = spawnPoints[spawnIndex];
+
+            GameObject fruit = Instantiate(fruitPrefab, spawnPoint.position, spawnPoint.rotation);
+            Destroy(fruit, 5f);
+        }
+    }
+}
